6aa04929fd3e79d2f49672c24470a7f524e4b096
Katharina Shits <1420757@stud.hs-mannheim.de>
2017-05-11 14:53:42 +0200

    changes at add.js

--- Testat1/add.js
+++ Testat1/add.js
@@ -1,9 +1,33 @@
 function validateForm() {
-    var vorname = document.forms["addplayers"]["vorname"].value;
-    var name = document.forms["addplayers"]["name"].value;
+    var vorname = document.getElementById("vorname").value;
+    var name = document.getElementById("name").value;
+    var verein = document.getElementById("verein").value;
+    var hcoach = document.getElementById("hcoach").value;
+    var acoach = document.getElementById("acoach").value;
     
-    if (x == "") {
-        alert("Name must be filled out");
+    var alphabet[] = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    var[] alphabetG = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    
+    for(int i=0; i< =vorname.length; i++){
+        var zeichen = vorname.charAt(i);
+        var alpha = alphabet[i];
+        var alphaG = alphabetG[i];
+        
+        if(!(zeichen == alpha) ||!(zeichen == alphaG){
+            alert("Felder dürfen keine Sonderzeichen enthalten.")
+            return false;
+        }else{
+            return true;
+        }
+                                           }
+    if (vorname == "" || name == "" || verein == "" || hcoach ="" || acoach== "") {
+        alert("Felder dürfen nicht leer sein.");
         return false;
     }
+
+
+}
+
+function alert(var txt){
+    document.getElementById("notification").innerHTML = txt;
 }
