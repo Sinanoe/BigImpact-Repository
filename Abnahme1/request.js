aa0c6d7cd131aabe519904619bd58d8d3336c251
Sinanoe <Sinan@Sinans-MacBook-Pro.local>
2017-05-11 16:58:21 +0200

    request/Abnahme

--- Abnahme1/request.js
+++ Abnahme1/request.js
@@ -1,17 +1,28 @@
-function myFunction(){   
+function myFunction() {
 
-var formData = new FormData();
-	formData.append('username', 'johndoe');
-	formData.append('id', 123456);
+    var formData = new FormData();
+    var inputs = document.getElementsByTagName('input');
+    var select = document.getElementsByTagName('select')[0];
+   
+    for (var i = 0; i < inputs.length; i++) {
+            console.log(inputs[i]);  
+        formData.append(inputs[i].name,inputs[i].value);
+    }
+        formData.append('position', select.options[select.selectedIndex].value);
+    
 
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', 'http://188.166.165.74:13337', true);
-	xhr.onload = function(e) {
-        
+    
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open('POST', 'http://188.166.165.74:13337/api/players', true);
+    //xhr.setRequestHeader('Access-Control-Allow-Origin','*');
+    xhr.setRequestHeader('Content-Type','application/json');
+    //xhr.setRequestHeader('Accept','*/*');
+    xhr.onload = function (e) {
+       
     };
 
-	xhr.send(formData);
-                              
-}
-                              
+    xhr.send(formData);
+
+}                
                               
