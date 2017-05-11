3733c81304c69e9f9f72c35eaf629aa7a51ee7d8
Katharina Shits <1420757@stud.hs-mannheim.de>
2017-05-11 13:31:40 +0200

    added add.js javascript file

---
+++ Testat1/add.js
@@ -0,0 +1,9 @@
+function validateForm() {
+    var vorname = document.forms["addplayers"]["vorname"].value;
+    var name = document.forms["addplayers"]["name"].value;
+    
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
