84943521b09fc2c24e69d1529efd068482f72b0a
Katharina Shits <1420757@stud.hs-mannheim.de>
2017-05-16 19:03:16 +0200

    javascript code changes.

--- Homepage/add.js
+++ Homepage/add.js
@@ -5,6 +5,10 @@ function validateForm() {
     var verein = document.getElementById("verein").value;
     var hcoach = document.getElementById("hcoach").value;
     var acoach = document.getElementById("acoach").value;
+    var nummer = document.getElementById("number").value;
+    var geburtsjahr = document.getElementById("jahr").value;
+    
+    
     var check = true;
 
     if(!vorname.match(/^[a-zA-zöaüßÖÄÜ]+$/)){
@@ -12,6 +16,20 @@ function validateForm() {
         check=false;
     }else{
         document.getElementById("vornameNotification").innerHTML = "";
+    }
+    
+    if((nummer<=3)&&(nummer>=16)){
+        document.getElementById("numberNotification").innerHTML = "Bitte &uumlberpr&uumlfe diese Eingabe.";
+        check=false;
+    }else{
+        document.getElementById("numberNotification").innerHTML = "";
+    }
+    
+    if(geburtsjahr<=0&&geburtsjahr>=2015){
+        document.getElementById("yearNotification").innerHTML = "Bitte &uumlberpr&uumlfe diese Eingabe.";
+        check=false;
+    }else{
+        document.getElementById("yearNotification").innerHTML = "";
     }
     
     if(!name.match(/^[a-zA-zöaüßÖÄÜ]+$/)){
@@ -43,7 +61,8 @@ function validateForm() {
     }
    
     if(check==true){
-        sendData();    
+        sendData();
+        alert("Information send.")
     }
 }
 
@@ -54,7 +73,6 @@ function sendData() {
     var select = document.getElementsByTagName('select')[0];
    
     for (var i = 0; i < inputs.length; i++) {
-            console.log(inputs[i]);  
         formData.append(inputs[i].name,inputs[i].value);
     }
         formData.append('position', select.options[select.selectedIndex].value);
@@ -73,4 +91,51 @@ function sendData() {
 
     xhr.send(formData);
 
+}
+
+function receiveData(){
+    
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open('GET', 'http://188.166.165.74:13337/api/players', true);
+    xhr.responseType= 'json';
+    xhr.onload = function(){
+        var data = xhr.response;
+    
+        if(data != null){
+            //TODO: Tabelle löschen
+            
+            //Tabelle erstellen
+            createTable(data);
+        }else{
+            console.log(data);
+        }
+    }
+    
+    xhr.send(null);
+   
+}
+
+function createTable(data){
+    var myTable = document.createElement("table"); 
+    var mytablebody = document.createElement("tbody");
+    
+    for(var i=0; i<=data.length; i++){
+        mycurrent_row = document.createElement("tr");
+        mytablebody.appendChild(mycurrent_row); 
+    
+    for(var j = 0; j <= 8; j++) { 
+        mycurrent_cell = document.createElement("td");                
+        currentname = document.createTextNode("Hallo" + data[j].name);
+        currentverein = document.createTextNode("Hallo" + data[j].name);
+        currentvorname = document.createTextNode("Hallo" + data[j].name);
+        current = document.createTextNode("Hallo" + data[j].name);
+        currenttext = document.createTextNode("Hallo" + data[j].name);
+        currenttext = document.createTextNode("Hallo" + data[j].name);
+        mycurrent_cell.appendChild(currenttext); 
+        mycurrent_row.appendChild(mycurrent_cell); 
+    }}
+
+    myTable.appendChild(mytablebody); 
+    document.getElementById('tabelle1').appendChild(myTable); 
 }
